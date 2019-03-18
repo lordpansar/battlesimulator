0e56a9b6ba5ef48091afbee8ee09a741019ad555
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-18 17:10:32 +0100

    Added test class + tests for warrior service

---
+++ BattleSimulator.Tests/WarriorTests.cs
@@ -0,0 +1,41 @@
+using BattleSimulator.Models;
+using BattleSimulator.Services;
+using System;
+using Xunit;
+
+namespace BattleSimulator.Tests
+{
+    public class WarriorTests
+    {
+        WarriorService service;
+
+        public WarriorTests()
+        {
+            service = new WarriorService();
+        }
+
+        [Fact]
+        public void AssertThatValuesAreRandomized()
+        {
+            bool isInRange = false;
+
+            var returnValue = service.Randomize(1, 5);
+
+            if(returnValue >= 1 && returnValue <= 5)
+            {
+                isInRange = true;
+            }
+
+            Assert.True(isInRange);
+        }
+
+        [Fact]
+        public void AssertThatWarriorCanBeCreated()
+        {
+            var warrior = service.SpawnNewWarrior(1);
+            var expected = typeof(Warrior);
+
+            Assert.IsType(expected, warrior);    
+        }
+    }
+}
