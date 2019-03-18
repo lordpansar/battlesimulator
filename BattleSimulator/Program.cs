9fc1e4b7032efc42663980a11e5eb7992d428f4c
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-18 10:14:53 +0100

    Add project files.

---
+++ BattleSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
