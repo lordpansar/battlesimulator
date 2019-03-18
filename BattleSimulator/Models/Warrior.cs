dbcfdb7823f352706b69a74fc50144ccfbedfa30
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-18 17:09:43 +0100

    Added warrior model + logic for spawning warrior objects

---
+++ BattleSimulator/Models/Warrior.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleSimulator.Models
+{
+    public class Warrior
+    {
+        public int Id { get; set; }
+        public int MinAttack { get; set; }
+        public int MaxAttack { get; set; }
+        public int MinDefense { get; set; }
+        public int MaxDefense { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
---
+++ BattleSimulator/Services/WarriorService.cs
@@ -0,0 +1,30 @@
+﻿using BattleSimulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleSimulator.Services
+{
+    public class WarriorService
+    {
+        public object SpawnNewWarrior(int id)
+        {
+            return new Warrior
+            {
+                Id = id,
+                MinAttack = Randomize(1, 3),
+                MaxAttack = Randomize(4, 8),
+                MinDefense = Randomize(2, 4),
+                MaxDefense = Randomize(5, 8),
+                IsAlive = true
+            };
+        }
+
+        public int Randomize(int min, int max)
+        {
+            var random = new Random();
+
+            return random.Next(min, max);
+        }
+    }
+}
