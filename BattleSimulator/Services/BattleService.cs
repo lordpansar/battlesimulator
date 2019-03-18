536ffe835ee282942b22eec844dea6d5841f1e2b
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-18 10:44:48 +0100

    Added basic battle logic

--- BattleSimulator/Services/BattleService.cs
+++ BattleSimulator/Services/BattleService.cs
@@ -6,5 +6,18 @@ namespace BattleSimulator.Services
 {
     public class BattleService
     {
+        public bool Attack(int attack, int defense)
+        {
+            //Attack must be greater than defense to inflict damage
+            if(attack > defense)
+            {
+                return true;
+            }
+            //Defense must be greater than or equal to attack power to deflect attack
+            else
+            {
+                return false;
+            }
+        }
     }
 }
