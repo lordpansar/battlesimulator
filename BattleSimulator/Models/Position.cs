b2f1a3de655e6eb35c1160158be152dffc5a3ece
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-18 17:09:59 +0100

    Added position model

---
+++ BattleSimulator/Models/Position.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleSimulator.Models
+{
+    public class Position
+    {
+        public int Vertical { get; set; }
+        public int Horizontal { get; set; }
+    }
+}
