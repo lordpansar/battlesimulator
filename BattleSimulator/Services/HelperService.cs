5ded6da24e4270f84e2343c9a8044313a422946c
Magnus Sundström <kmvsundstrom@gmail.com>
2019-07-12 10:33:45 +0200

    Added HelperService class

---
+++ BattleSimulator/Services/HelperService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleSimulator.Services
+{
+    public static class Helper
+    {
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
