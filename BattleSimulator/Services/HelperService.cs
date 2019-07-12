6cd495c3bb9a2e5dc298947e48f96b954385abdc
Magnus Sundström <kmvsundstrom@gmail.com>
2019-07-12 11:13:12 +0200

    Renamed helper class and added randomize method

--- BattleSimulator/Services/HelperService.cs
+++ BattleSimulator/Services/HelperService.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BattleSimulator.Services
 {
-    public static class Helper
+    public static class HelperService
     {
         public static void Print(string message)
         {
             Console.WriteLine(message);
+        }
+
+        public static int Randomize(int min, int max)
+        {
+            var random = new Random();
+
+            return random.Next(min, max);
         }
     }
 }
