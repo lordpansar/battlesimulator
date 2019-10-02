9cf8cafcaf16aa60244a9250ba10bdf809daa481
Magnus Sundström <kmvsundstrom@gmail.com>
2019-10-02 15:23:30 +0200

    Added demo function

--- BattleSimulator/Program.cs
+++ BattleSimulator/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+using BattleSimulator.Models;
+using BattleSimulator.Services;
+using System;
+using System.Collections.Generic;
 
 namespace BattleSimulator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunTestFight();
+            Console.ReadLine();
+        }
+
+        static void RunTestFight()
+        {
+            WarriorService warriorService = new WarriorService();
+            BattleService battleService = new BattleService();
+            var warriors = new List<Warrior>();
+
+            var crogar = warriorService.SpawnWarrior(1);
+            var torulf = warriorService.SpawnWarrior(2);
+
+            warriors.Add(crogar);
+            warriors.Add(torulf);
+
+            battleService.CommenceBattle(warriors);
         }
     }
 }
