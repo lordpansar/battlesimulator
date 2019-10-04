84fd8ad966faae4eebe625dbc7853f32c53c7f6c
Magnus Sundström <kmvsundstrom@gmail.com>
2019-10-04 14:15:45 +0200

    Added logging to battle logic

--- BattleSimulator/Services/BattleService.cs
+++ BattleSimulator/Services/BattleService.cs
@@ -1,5 +1,7 @@
 ﻿using BattleSimulator.Models;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace BattleSimulator.Services
 {
@@ -16,7 +18,7 @@ namespace BattleSimulator.Services
                 first = warriors[0];
                 second = warriors[1];
             }
-            else if(second.Iniative > first.Iniative)
+            else if(warriors[1].Iniative > warriors[0].Iniative)
             {
                 //If second draws first
                 first = warriors[1];
@@ -42,28 +44,38 @@ namespace BattleSimulator.Services
             while (first.IsAlive && second.IsAlive)
             {
                 //Battle to death
+                Console.WriteLine($"{first.Name} furiously attacks!");
                 var defenderIsHurt = Attack(HelperService.Randomize(first.MinAttack, first.MaxAttack), HelperService.Randomize(second.MinDefense, second.MaxDefense));
 
                 if (defenderIsHurt)
                 {
                     second.IsAlive = false;
+                    Console.WriteLine($"{first.Name} viciously slays {second.Name}. May he feast forever in Valhalla.\n");
                     break;
                 }
                 else
                 {
+                    Console.WriteLine($"Haha! {first.Name}'s puny sword is no match for {second.Name}'s mighty armour!");
+                    Console.WriteLine($"{second.Name} furiously attacks!");
+                    
                     defenderIsHurt = Attack(HelperService.Randomize(second.MinAttack, second.MaxAttack), HelperService.Randomize(first.MinDefense, first.MaxDefense));
 
                     if (defenderIsHurt)
                     {
                         first.IsAlive = false;
+                        Console.WriteLine($"{second.Name} viciously slays {first.Name}. May he feast forever in Valhalla.\n");
                         break;
                     }
+                    
+                    Console.WriteLine($"Haha! {second.Name}'s puny sword is no match for {first.Name}'s mighty armour!");
                 }
             }
         }
 
         public bool Attack(int attack, int defense)
         {
+            Console.WriteLine($"Attack: {attack} Defense: {defense}");
+
             //Attack must be greater than defense to inflict damage
             if(attack > defense)
             {
