72da38b2847f033a6b9a08c580126f5117183303
Magnus Sundström <kmvsundstrom@gmail.com>
2019-10-04 14:15:32 +0200

    Added name generation

--- BattleSimulator/Services/WarriorService.cs
+++ BattleSimulator/Services/WarriorService.cs
@@ -10,6 +10,7 @@ namespace BattleSimulator.Services
             return new Warrior
             {
                 Id = id,
+                Name = GenerateName(),
                 Iniative = HelperService.Randomize(1,10),
                 MinAttack = HelperService.Randomize(1, 3),
                 MaxAttack = HelperService.Randomize(4, 8),
@@ -17,6 +18,91 @@ namespace BattleSimulator.Services
                 MaxDefense = HelperService.Randomize(5, 8),
                 IsAlive = true
             };
+        }
+
+        public string GenerateName()
+        {
+            var first = HelperService.Randomize(1, 10);
+            var second = HelperService.Randomize(1, 10);
+
+            var firstName = "";
+            string lastName = "";
+
+            switch (first)
+            {
+                case 1:
+                    firstName = "Crogar";
+                    break;
+                case 2:
+                    firstName = "Torulf";
+                    break;
+                case 3:
+                    firstName = "Grognak";
+                    break;
+                case 4:
+                    firstName = "Conan";
+                    break;
+                case 5:
+                    firstName = "Hagrid";
+                    break;
+                case 6:
+                    firstName = "Stoneface";
+                    break;
+                case 7:
+                    firstName = "Edgar";
+                    break;
+                case 8:
+                    firstName = "Thy Dungeonman";
+                    break;
+                case 9:
+                    firstName = "Grokk";
+                    break;
+                case 10:
+                    firstName = "Bork";
+                    break;
+                default:
+                    firstName = "Mr Alias";
+                    break;
+            }
+
+            switch (second)
+            {
+                case 1:
+                    lastName = "the Horrible";
+                    break;
+                case 2:
+                    lastName = "Swordsbane";
+                    break;
+                case 3:
+                    lastName = "the Fat";
+                    break;
+                case 4:
+                    lastName = "Gutmuncher";
+                    break;
+                case 5:
+                    lastName = "the Awful";
+                    break;
+                case 6:
+                    lastName = "Armbreaker";
+                    break;
+                case 7:
+                    lastName = "the Terrible";
+                    break;
+                case 8:
+                    lastName = "von Hell";
+                    break;
+                case 9:
+                    lastName = "the Smelly";
+                    break;
+                case 10:
+                    lastName = "Thunderaxe";
+                    break;
+                default:
+                    lastName = "Undercover";
+                    break;
+            }
+
+            return $"{firstName} {lastName}";
         }
     }
 }
